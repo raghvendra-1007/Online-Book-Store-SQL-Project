486bf1f70854fbbed59caa48f88faa1b14b6ede0
Raghvendra Sharma <raghvendra2246@gmail.com>
2025-06-06 00:38:48 +0530

    Update 20..sql

--- Queries-file/20..sql
+++ Queries-file/20..sql
@@ -1,4 +1,4 @@
-Calculate the stock remaining after fulfilling all orders
+#Calculate the stock remaining after fulfilling all orders
 
 SELECT Books.Title, (Books.Stock - SUM(Orders.Quantity)) AS "REMAINING STOCK"
 FROM Books
